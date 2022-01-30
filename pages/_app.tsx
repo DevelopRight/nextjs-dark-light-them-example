774cdf292546ef3bb76581bff93f8907cf8827a5
Connor Mcgarty-Wood <connormcwood2@gmail.com>
2022-01-30 20:27:45 +0000

    push changes for dark light theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,43 @@
+import { useEffect, useState } from 'react';
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+
+const lightTheme = createTheme({
+  palette: {
+    mode: 'light'
+  }
+})
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark'
+  }
+})
+
+function getActiveTheme(themeMode: 'light' | 'dark') {
+  return themeMode === 'light' ? lightTheme : darkTheme;
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const [activeTheme, setActiveTheme] = useState(lightTheme);
+  const [selectedTheme, setSelectedTheme] = useState<'light' | 'dark'>('light');
+
+  const toggleTheme: React.MouseEventHandler<HTMLAnchorElement> = () => {
+    const desiredTheme = selectedTheme === 'light' ? 'dark' : 'light';
+
+    setSelectedTheme(desiredTheme);
+  };
+
+  useEffect(() => {
+    setActiveTheme(getActiveTheme(selectedTheme))
+  }, [selectedTheme]);
+
+  return (
+    <ThemeProvider theme={activeTheme}>
+      <Component {...pageProps} toggleTheme={toggleTheme} />
+    </ThemeProvider>
+  )
 }
 
 export default MyApp
