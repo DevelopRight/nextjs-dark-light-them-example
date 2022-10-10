8a31a20cf750fa1a4656a4ee4dca125ca98e4c68
Connor Mcgarty-Wood <connormcgartywood@rideways.com>
2022-10-10 18:25:04 +0100

    chore: app config

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from 'react';
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { useCookies } from 'react-cookie';
+import { useMediaQuery } from '@mui/material';
 
 const lightTheme = createTheme({
   palette: {
@@ -19,20 +21,30 @@ function getActiveTheme(themeMode: 'light' | 'dark') {
   return themeMode === 'light' ? lightTheme : darkTheme;
 }
 
+const PREFERENCE_COOKIE_NAME = 'theme-preference'
+
 function MyApp({ Component, pageProps }: AppProps) {
+  const userSystemThemePreferenceDark = useMediaQuery('(prefers-color-scheme: dark)');
+  
   const [activeTheme, setActiveTheme] = useState(lightTheme);
-  const [selectedTheme, setSelectedTheme] = useState<'light' | 'dark'>('light');
+  const [cookieTheme, setCookieTheme] = useCookies([PREFERENCE_COOKIE_NAME]);
+
+  const defaultInitialTheme = userSystemThemePreferenceDark ? 'dark' : 'light';
+  const preferredTheme = cookieTheme && cookieTheme[PREFERENCE_COOKIE_NAME] ? cookieTheme[PREFERENCE_COOKIE_NAME] : defaultInitialTheme;
+
+  const [selectedTheme, setSelectedTheme] = useState<'light' | 'dark'>(preferredTheme);
 
   const toggleTheme: React.MouseEventHandler<HTMLAnchorElement> = () => {
     const desiredTheme = selectedTheme === 'light' ? 'dark' : 'light';
 
     setSelectedTheme(desiredTheme);
+    setCookieTheme(PREFERENCE_COOKIE_NAME, desiredTheme);
   };
 
   useEffect(() => {
     setActiveTheme(getActiveTheme(selectedTheme))
   }, [selectedTheme]);
-
+  
   return (
     <ThemeProvider theme={activeTheme}>
       <Component {...pageProps} toggleTheme={toggleTheme} />
